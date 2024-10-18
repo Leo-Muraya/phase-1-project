bea963d81c39320c6c2fa06d586d5919bce94c01
Leguminous <murayaleo911@gmail.com>
2024-10-18 14:05:46 +0300

    done

--- script.js
+++ script.js
@@ -50,3 +50,14 @@ function showDetails(recipe) {
     `;
     recipeDetails.style.display = 'block';
 }
+
+//Navigation bar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
